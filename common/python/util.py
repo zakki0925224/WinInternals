399fe5bdda86757c1b3e17b12409035eb1220e23
zakki0925224 <zakki0925224@gmail.com>
2026-06-26 16:41:05 +0900

    [Add]: Added OOB exploitation example

--- common/python/util.py
+++ common/python/util.py
@@ -1,6 +1,7 @@
 import os
 import socket
 import struct
+import subprocess
 
 
 def find_exe(script_path: str, name: str) -> str:
@@ -32,16 +33,20 @@ class Socket:
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = int(port)
-        self.sock = None
+        self.sock: socket.socket | None = None
 
     def connect(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((self.host, self.port))
 
     def send(self, data: bytes):
+        assert self.sock
+
         self.sock.sendall(data)
 
     def recvuntil(self, delimiter: bytes) -> bytes:
+        assert self.sock
+
         buf = b""
         while not buf.endswith(delimiter):
             chunk = self.sock.recv(1)
@@ -54,3 +59,37 @@ class Socket:
         if self.sock:
             self.sock.close()
             self.sock = None
+
+
+class Process:
+    def __init__(self, cmd):
+        self.cmd = cmd
+        self.proc: subprocess.Popen[bytes] | None = None
+
+    def connect(self):
+        self.proc = subprocess.Popen(
+            self.cmd,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+        )
+
+    def send(self, data: bytes):
+        assert self.proc and self.proc.stdin
+        self.proc.stdin.write(data)
+        self.proc.stdin.flush()
+
+    def recvuntil(self, delimiter: bytes) -> bytes:
+        assert self.proc and self.proc.stdout
+        buf = b""
+        while not buf.endswith(delimiter):
+            chunk = self.proc.stdout.read(1)
+            if not chunk:
+                raise ConnectionError("Process closed before delimiter was received")
+            buf += chunk
+        return buf
+
+    def kill(self):
+        if self.proc:
+            self.proc.terminate()
+            self.proc = None
--- common/python/winenv.py
+++ common/python/winenv.py
@@ -1,1 +1,4 @@
 SHELL_CODE = b"\xcc\xcc\xcc\xcc"  # INT3 x4
+
+X86_OFFSET_BaseThreadInitThunk = 0x1FCB0
+X86_OFFSET_VirtualProtectStub = 0x20760
